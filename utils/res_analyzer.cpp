d252059071befa12ded621c6683491443fd3d104
GSA7 <isgaosa@gmail.com>
2018-04-15 13:51:45 +0800

    add cdf analyze

--- utils/res_analyzer.cpp
+++ utils/res_analyzer.cpp
@@ -36,13 +36,27 @@ int main() {
         cout << name[i] << ":" << endl;
         int ae = 0, acc = 0;
         double re = 0;
+        int cdf[10][2];
+        for (int j = 0; j < 20; ++j) {
+            cdf[j/2][j%2] = 0;
+        }
         for (const auto& recc: rec) {
             ae += recc[i].ae;
             acc += recc[i].acc;
             re += recc[i].re;
+            for (int j = 0; j < 10; ++j) {
+                if (recc[i].ae <= j) ++cdf[j][0];
+                if (recc[i].re <= (double)j / 10) ++cdf[j][1];
+            }
         }
         cout << "aae: " << (double)ae / rec.size() << endl;
         cout << "are: " << (double)re / rec.size() << endl;
         cout << "acc: " << (double)acc / rec.size() << endl;
+        cout << "ae-cdf:";
+        for (int j = 0; j < 10; ++j) cout << " " << (double)cdf[j][0] / rec.size();
+        cout << endl;
+        cout << "re-cdf:";
+        for (int j = 0; j < 10; ++j) cout << " " << (double)cdf[j][1] / rec.size();
+        cout << endl;
     }
 }
