6e681b527840923eb7b791ee9da756a6a20b4da5
GSA7 <isgaosa@gmail.com>
2018-04-22 16:54:55 +0800

    change run1.sh

--- run1.sh
+++ run1.sh
@@ -14,3 +14,5 @@ python3 utils/res_analyzer.py res1.txt res1.pickle
 python3 utils/res_analyzer.py res2.txt res2.pickle
 python3 utils/res_analyzer.py res3.txt res3.pickle
 python3 utils/res_analyzer.py res4.txt res4.pickle
+
+python3 utils/res_grapher.py res1.pickle res2.pickle res3.pickle res4.pickle
